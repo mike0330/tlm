f91778a63474e6a884a29546b224009f79307b09
mike0330 <18513082073@163.com>
2021-07-21 08:22:46 +0800

    20210721

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>我的工作台</span>
+      </div>
+      <div class="card-body">
+        <div class="avatar">
+          <img src="" alt="">
+        </div>
+        <div class="perInfo">
+          NNNNNN
+        </div>
+      </div>
+    </el-card>
   </div>
+  
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Dashboard',
+  name: 'homePage',
   computed: {
     ...mapGetters([
       'name'
@@ -18,13 +31,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
+  .dashboard {
+    &-container {
+      margin: 30px;
+    }
+    &-text {
+      font-size: 30px;
+      line-height: 46px;
+    }
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
+
+  .card-body{
+    display: flex;
   }
-}
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+  .box-card {
+    width: 60%;
+  }
 </style>
